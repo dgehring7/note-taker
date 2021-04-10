7050954be9d6a6a21b604ec230422c6c65f0fc8e
Dgehring <dgehring7@gmail.com>
2021-04-10 15:37:31 -0400

    updated post request in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,12 +9,30 @@ router.get("/notes", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-router.post("/notes", (req, res) => {
-    noteStore.saveNote(req.body)
-    .then((note) =>
-        res.json(note))
-        .catch((err) => res.status(500).json(err));
-});
+// router.post("/notes", (req, res) => {
+//     noteStore.saveNote(req.body)
+//     .then((note) =>
+//         res.json(note))
+//         .catch((err) => res.status(500).json(err));
+// });
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = (notes.length + 1);
+    fs.readFile(db, 'utf8', (err, data) => {
+      // err handle
+      // parse data
+      const thisNote = fs.readFileSync(path.join(__dirname, 'db/db.json'));
+      const notes = JSON.parse(thisNote);
+      // add new Note with data.push
+      notes.push(newNote);
+      // stringify data
+      const response = JSON.stringify(notes);
+      fs.writeFileSync(path.join(__dirname, '/db/db.json') (err), response);
+        // end response, send response
+        res.json(newNote);
+      })
+    })
 
 router.delete("/notes", (req, res) => {
     noteStore.deleteNote()
